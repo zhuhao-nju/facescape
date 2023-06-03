3dfdff32de4285fb17f624359abe40d0457fb375
zhuhao-nju <40872587+zhuhao-nju@users.noreply.github.com>
2023-06-04 03:22:08 +0800

    Update download_data.sh

--- benchmark/data/download_data.sh
+++ benchmark/data/download_data.sh
@@ -5,7 +5,7 @@ fi
 
 # download
 if [ ! -f ./data.zip ] ; then
-wget --no-check-certificate 'https://box.nju.edu.cn/f/30d0657e93b345ec88fa/?dl=1' -O ./data.zip
+wget --no-check-certificate 'https://box.nju.edu.cn/f/568df86113054a06a0da/?dl=1' -O ./data.zip
 fi
 
 # extract files
